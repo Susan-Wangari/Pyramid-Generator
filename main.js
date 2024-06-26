e58c2f7633d1ed5cb88f2bfd1da905ebfa9e00f0
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2024-06-26 13:21:08 +0300

    repeat to arrange the pyramid structure

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const character = "#";
 const count = 8;
 const rows = [];
+
+//used to create a loop to count the number of characters in the pyramid
+//repeat helps you show/log the same string the number of times in the brackets e.g 3 times or in our case i times
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i));
+}
